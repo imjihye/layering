6f14a45e2cf05bbad428b47e66ba48e09c6aa4ba
jihye <im@jihye.me>
2017-02-12 13:00:39 +0900

    add layering

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,39 @@
 package com.example;
 
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.List;
+
 /**
  * Created by kimjihye on 2017. 2. 12..
  */
-public class App {
+
+@EnableAutoConfiguration
+@ComponentScan
+public class App implements CommandLineRunner {
+    @Autowired // DI컨테이너가 엔트리 포인트에 서비스를 주입한다.
+    CustomerService customerService;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        customerService.save(new Customer(1, "A", "AA"));
+        customerService.save(new Customer(2, "B", "BB"));
+        customerService.save(new Customer(3, "C", "CC"));
+
+        // customerService.findAll().forEach(System.out::println);
+        List<Customer> customerList = customerService.findAll();
+        for(Customer customer: customerList) {
+            System.out.println(customer.toString());
+        }
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
 }
--- src/main/java/com/example/domain/Customer.java
+++ src/main/java/com/example/domain/Customer.java
@@ -4,10 +4,17 @@ package com.example.domain;
  * Created by kimjihye on 2017. 2. 12..
  */
 
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Customer {
     private Integer id;
     private String firstName;
     private String lastName;
+
+    public Customer(){
+
+    }
 
     public Customer(Integer id, String firstName, String lastName) {
         this.id = id;
